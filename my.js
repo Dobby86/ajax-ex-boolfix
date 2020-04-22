ff4a428b8c9fa76cf874be98e84f29ab0dcca358
marco <dobermannpoker86@gmail.com>
2020-04-22 12:23:25 +0200

    aggiunta di flags,e doppio ajax

--- my.js
+++ my.js
@@ -8,12 +8,14 @@
 $(document).ready(function() {
 // al click
 
-$("button").click(function() {
-
+$("#cerca").click(function() {
+// come resettare la pagina
     $(".container").html(" ");
+
+
     var variabileRicerca = $("#scrivi").val();
 
-
+// listaFilm
 $.ajax({
 
     url: "https://api.themoviedb.org/3/search/movie",
@@ -28,33 +30,96 @@ $.ajax({
     success : function (data) {
 
         console.log(data);
-
+ // sourc e compile
     var source = $("#entry-template").html();
-     var template = Handlebars.compile(source);
+    var template = Handlebars.compile(source);
 
-        var listaFilm = data.results;
+    var listaFilm = data.results;
+// ciclooo......
+
 
         for (var i = 0; i < listaFilm.length; i++) {
+            // lingue
+            var ita = '<img src = "img/it.png" alt="">';
+            var en = '<img src = "img/gb.png" alt="">';
+            var es = '<img src = "img/es.png" alt="">';
+
             var listaCorrente = listaFilm [i];
 
             var context = {
-
+                // cover : listaCorrente.cover_path,
                 title : listaCorrente.title,
                 subtitle : listaCorrente.original_title,
-                language :listaCorrente.original_language,
-                voto : listaCorrente.vote_count
+                language : ita,
+                vote : listaCorrente.vote_average
 
             };
+
             var risultatoDaAggiungere = template(context);
             $(".film-container").append(risultatoDaAggiungere);
             console.log(risultatoDaAggiungere);
         }
 
     },
+
       error: function(richiesta, stato, errori){
-    }
+    },
+
+
+});
+// serie tv
+$.ajax({
+
+    url: "https://api.themoviedb.org/3/search/tv",
+    method: "GET",
+    dataType: "json",
+    data: {
+        api_key: "76dafb6359bcac21006df98494394fce",
+        language: "it-IT",
+        query: variabileRicerca,
+    },
+    // handlebars
+    success : function (data) {
+
+        console.log(data);
+ // sourc e compile
+    var source = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+
+    var listaSerie = data.results;
+// ciclooo......
+ // primaFunzione(listaFilm);
+
+        for (var i = 0; i < listaSerie.length; i++) {
+            // lingue
+            var ita = '<img src = "img/it.png" alt="">';
+            var en = '<img src = "img/gb.png" alt="">';
+            var es = '<img src = "img/es.png" alt="">';
+
+            var listaCorrente = listaSerie [i];
+
+            var context = {
+                
+                title : listaCorrente.name,
+                subtitle : listaCorrente.original_name,
+                language : ita,
+                vote : listaCorrente.vote_count
+
+            };
+
+            var risultatoDaAggiungere = template(context);
+            $(".film-container").append(risultatoDaAggiungere);
+            console.log(risultatoDaAggiungere);
+        }
+
+    },
+
+      error: function(richiesta, stato, errori){
+    },
+
 
 });
 
 });
+// fine ready
 });
