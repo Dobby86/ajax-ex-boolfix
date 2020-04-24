31c04bae698d0a4ad91090fa8e49164b1418dcdd
marco <dobermannpoker86@gmail.com>
2020-04-24 17:39:45 +0200

    fixato qualcosa dell js

--- my.js
+++ my.js
@@ -6,8 +6,33 @@
 // Lingua
 // Voto
 $(document).ready(function() {
-// al click
+//mouse enter
+$(document).on('mouseenter', '.film', function() {
 
+                    // console.log('sono entrato in un film!');
+                    var elementoSelezionato = $(this);
+                    var img = elementoSelezionato.find('.copertina');
+
+                    // console.log(img.attr('src'));
+                    $(this).find(".overlay").show();
+                    $(this).find(".backimg").show();
+
+
+});
+//mouse leave
+$(document).on('mouseleave', '.film', function() {
+
+                    // console.log('sono entrato in un film!');
+                    var elementoSelezionato = $(this);
+                    var img = elementoSelezionato.find('.copertina');
+                    // console.log(img.attr('src'));
+
+    $(this).find(".overlay").hide();
+
+
+});
+
+// al click
 $("#cerca").click(function() {
 // come resettare la pagina
     $(".container").html(" ");
@@ -16,94 +41,99 @@ $("#cerca").click(function() {
     var variabileRicerca = $("#scrivi").val();
 
 // listaFilm
-$.ajax({
+    $.ajax({
 
-            url: "https://api.themoviedb.org/3/search/movie",
-            method: "GET",
-            dataType: "json",
-            data: {
-                api_key: "76dafb6359bcac21006df98494394fce",
-                language: "it-IT",
-                query: variabileRicerca,
-                    },
-    // handlebars
-    success : function (data) {
+                url: "https://api.themoviedb.org/3/search/movie",
+                method: "GET",
+                dataType: "json",
+                data: {
+                        api_key: "76dafb6359bcac21006df98494394fce",
+                        language: "it-IT",
+                        query: variabileRicerca,
+                        },
+                // handlebars
+                success : function (data) {
 
-        console.log(data);
- // sourc e compile
-    var source = $("#entry-template").html();
-    var template = Handlebars.compile(source);
+                    console.log(data);
+             // sourc e compile
+                var source = $("#entry-template").html();
+                var template = Handlebars.compile(source);
 
-    var listaFilm = data.results;
+                var listaFilm = data.results;
 // ciclooo......
 
 
         for (var i = 0; i < listaFilm.length; i++) {
 
-            var listaCorrente = listaFilm [i];
+                        var listaCorrente = listaFilm [i];
 
-            //parte simile
-            var originalLan = listaCorrente.original_language;
+                        //parte simile
+                        var originalLan = listaCorrente.original_language;
 
-            var valoreProprietàLinguaggio = "";
+                        var valoreProprietàLinguaggio = "";
 
-            console.log(originalLan);
+                        console.log(originalLan);
 
             // come lo valorizzo?
             if (originalLan == "en") {
-                valoreProprietàLinguaggio = '<img class="flag" src = "img/gb.png" >';
+                        valoreProprietàLinguaggio = '<img class="flag" src = "img/gb.png" >';
 
             }else if (originalLan == "it"){
-                valoreProprietàLinguaggio = '<img class="flag" src = "img/it.png" >';
+                                    valoreProprietàLinguaggio = '<img class="flag" src = "img/it.png" >';
 
             }else if (originalLan == "es"){
-                valoreProprietàLinguaggio = '<img class="flag" src = "img/es.png" >';
+                        valoreProprietàLinguaggio = '<img class="flag" src = "img/es.png" >';
 
             }else if (originalLan == "de"){
-                valoreProprietàLinguaggio = '<img class="flag" src = "img/de.png" >';
+                        valoreProprietàLinguaggio = '<img class="flag" src = "img/de.png" >';
             }else if (originalLan == "fr"){
-                valoreProprietàLinguaggio = '<img class="flag" src = "img/fr.png" >';
+                        valoreProprietàLinguaggio = '<img class="flag" src = "img/fr.png" >';
             }else if (originalLan == "ja"){
-                valoreProprietàLinguaggio = '<img class="flag" src = "img/jp.png" >';
+                        valoreProprietàLinguaggio = '<img class="flag" src = "img/jp.png" >';
             }else if (originalLan == "sv"){
-                valoreProprietàLinguaggio = '<img class="flag"  src = "img/se.png" >';
+                        valoreProprietàLinguaggio = '<img class="flag"  src = "img/se.png" >';
             }
              else {
-                valoreProprietàLinguaggio = originalLan;
+                        valoreProprietàLinguaggio = originalLan;
             }
             // partefilm
-            var voto = listaCorrente.vote_average;
-            var votoCorretto = Math.ceil(voto / 2);
-            // copertine
-            var sito = "https://image.tmdb.org/t/p/";
-            var size = "w185";
-            var coverFilm = sito + size + listaCorrente.poster_path ;
-            // parte copertine
-            var imgRotta = "https://lh3.googleusercontent.com/proxy/zDQaGTu0edCNxXuCnASMhw8GxMsy_7ic-CJU16sTsYdPhz2UzJzVRkluZphOaZTM0GNve1nLc8mGBHnai7cjNxCDQ-CLWjhsRlTRxcJ1DRwjO5u8RzDl";
+                                var voto = listaCorrente.vote_average;
+                                var votoCorretto = Math.ceil(voto / 2);
+                                // copertine
+                                var sito = "https://image.tmdb.org/t/p/";
+                                var size = "w185";
+                                var coverFilm = sito + size + listaCorrente.poster_path ;
+
+                                // parte copertine
+                                var imgRotta = "https://lh3.googleusercontent.com/proxy/zDQaGTu0edCNxXuCnASMhw8GxMsy_7ic-CJU16sTsYdPhz2UzJzVRkluZphOaZTM0GNve1nLc8mGBHnai7cjNxCDQ-CLWjhsRlTRxcJ1DRwjO5u8RzDl";
 
 
             var verificare = listaCorrente.poster_path;
 
-                 if (verificare == null) {
+                        if (verificare == null) {
                      generator = imgRotta ;
-                 } else {
+                        } else {
                      generator = coverFilm;
                  }
+                 // cambio copertina......................................................................
 
             var context = {
 
-                title : listaCorrente.title,
-                subtitle : listaCorrente.original_title,
-                date : listaCorrente.release_date,
-                language :valoreProprietàLinguaggio,
-                cover : generator,
-                stelline : generaStelle(votoCorretto),
-                tipoRichiesta : "Film"
-                // voto : votoCorretto
-            };
-            var risultatoDaAggiungere = template(context);
-            $(".film-container").append(risultatoDaAggiungere);
-            // console.log(risultatoDaAggiungere);
+                         back:listaCorrente.backdrop_path,
+
+                        title : listaCorrente.title,
+                        subtitle : listaCorrente.original_title,
+                        date : listaCorrente.release_date,
+                        language :valoreProprietàLinguaggio,
+                        cover : generator,
+                        overview : listaCorrente.overview,
+                        stelline : generaStelle(votoCorretto),
+                        tipoRichiesta : "Film"
+
+                    };
+                        var risultatoDaAggiungere = template(context);
+                        $(".film-container").append(risultatoDaAggiungere);
+                        // console.log(risultatoDaAggiungere);
         }
 
     },
@@ -113,32 +143,33 @@ $.ajax({
     },
 
 });
+
 // serie tv........................................................................................................
 $.ajax({
 
-    url: "https://api.themoviedb.org/3/search/tv",
-    method: "GET",
-    dataType: "json",
-    data: {
-        api_key: "76dafb6359bcac21006df98494394fce",
-        language: "it-IT",
-        query: variabileRicerca,
+            url: "https://api.themoviedb.org/3/search/tv",
+            method: "GET",
+            dataType: "json",
+            data: {
+                api_key: "76dafb6359bcac21006df98494394fce",
+                language: "it-IT",
+                query: variabileRicerca,
     },
     // handlebars
     success : function (data) {
 
         console.log(data);
  // sourc e compile
-    var source = $("#entry-template").html();
-    var template = Handlebars.compile(source);
+                            var source = $("#entry-template").html();
+                            var template = Handlebars.compile(source);
 
-    var listaSerie = data.results;
+                            var listaSerie = data.results;
 
         for (var i = 0 ;  i < listaSerie.length; i++) {
 
-            var listaCorrenteSerie = listaSerie [i];
+                    var listaCorrenteSerie = listaSerie [i];
 
-            var originalLan = listaCorrenteSerie.original_language;
+                    var originalLan = listaCorrenteSerie.original_language;
 
  // listaCorrenteSerie.original_language;
 
@@ -147,64 +178,64 @@ $.ajax({
             console.log(originalLan);
 
             // come lo valorizzo?
-            if (originalLan == "en") {
-                valoreProprietàLinguaggio = '<img class="flag" src = "img/gb.png">';
-            }else if (originalLan == "it"){
-                valoreProprietàLinguaggio = '<img class="flag" src = "img/it.png" >';
-            }else if (originalLan == "es"){
-                valoreProprietàLinguaggio = '<img class="flag" src = "img/es.png" >';
-            }else if (originalLan == "de"){
-                valoreProprietàLinguaggio = '<img class="flag" src = "img/de.png" >';
-            }else if (originalLan == "fr"){
-                valoreProprietàLinguaggio = '<img class="flag" src = "img/fr.png" >';
-            }else if (originalLan == "ja"){
-                valoreProprietàLinguaggio = '<img class="flag" src = "img/jp.png" >';
-            }else if (originalLan == "sv"){
-                valoreProprietàLinguaggio = '<img class="flag" src = "img/se.png" >';
-            }
-             else {
-                valoreProprietàLinguaggio = originalLan;
-            }
+                            if (originalLan == "en") {
+                                valoreProprietàLinguaggio = '<img class="flag" src = "img/gb.png">';
+                            }else if (originalLan == "it"){
+                                valoreProprietàLinguaggio = '<img class="flag" src = "img/it.png" >';
+                            }else if (originalLan == "es"){
+                                valoreProprietàLinguaggio = '<img class="flag" src = "img/es.png" >';
+                            }else if (originalLan == "de"){
+                                valoreProprietàLinguaggio = '<img class="flag" src = "img/de.png" >';
+                            }else if (originalLan == "fr"){
+                                valoreProprietàLinguaggio = '<img class="flag" src = "img/fr.png" >';
+                            }else if (originalLan == "ja"){
+                                valoreProprietàLinguaggio = '<img class="flag" src = "img/jp.png" >';
+                            }else if (originalLan == "sv"){
+                                valoreProprietàLinguaggio = '<img class="flag" src = "img/se.png" >';
+                            }
+                             else {
+                                valoreProprietàLinguaggio = originalLan;
+                            }
 
-            var voto = listaCorrenteSerie.vote_average;
-            var votoCorretto = Math.ceil(voto / 2);
-            // copertine serie
-            var sito = "https://image.tmdb.org/t/p/";
-            var size = "w185";
-            var coverSerie = sito + size + listaCorrenteSerie.poster_path ;
+                            var voto = listaCorrenteSerie.vote_average;
+                            var votoCorretto = Math.ceil(voto / 2);
+                            // copertine serie
+                            var sito = "https://image.tmdb.org/t/p/";
+                            var size = "w185";
+                            var coverSerie = sito + size + listaCorrenteSerie.poster_path ;
 
             // test
 
-            // parte immagini
-            var imgRotta = "https://lh3.googleusercontent.com/proxy/zDQaGTu0edCNxXuCnASMhw8GxMsy_7ic-CJU16sTsYdPhz2UzJzVRkluZphOaZTM0GNve1nLc8mGBHnai7cjNxCDQ-CLWjhsRlTRxcJ1DRwjO5u8RzDl";
+                            // parte immagini
+                            var imgRotta = "https://lh3.googleusercontent.com/proxy/zDQaGTu0edCNxXuCnASMhw8GxMsy_7ic-CJU16sTsYdPhz2UzJzVRkluZphOaZTM0GNve1nLc8mGBHnai7cjNxCDQ-CLWjhsRlTRxcJ1DRwjO5u8RzDl";
 
             var verificare = listaCorrenteSerie.poster_path;
 
-                 if (verificare == null) {
-                     generator = imgRotta ;
-                 } else {
-                     generator = coverSerie;
-                 }
+                             if (verificare == null) {
+                                 generator = imgRotta ;
+                             } else {
+                                 generator = coverSerie;
+                             }
 
             var context = {
 
-                title : listaCorrenteSerie.name,
-                subtitle : listaCorrenteSerie.original_name,
-                date : listaCorrenteSerie.first_air_date,
-                language : valoreProprietàLinguaggio,
-                stelline : generaStelle(votoCorretto),
-                cover : generator,
-                // cover : imgSiOno(coverSerie),
-                tipoRichiesta : "Tv Series"
-                // voto : votoCorretto
-            };
+                        title : listaCorrenteSerie.name,
+                        subtitle : listaCorrenteSerie.original_name,
+                        date : listaCorrenteSerie.first_air_date,
+                        language : valoreProprietàLinguaggio,
+                        stelline : generaStelle(votoCorretto),
+                        cover : generator,
+                        // cover : imgSiOno(coverSerie),
+                        tipoRichiesta : "Tv Series"
+                        // voto : votoCorretto
+                    };
 
             var risultatoDaAggiungere = template(context);
 
             $(".serie-container").append(risultatoDaAggiungere);
             // console.log(risultatoDaAggiungere);
         }
-    // }
+
 // fine faseprima
     },
 
@@ -224,12 +255,12 @@ function generaStelle (voto){
 
     var stelleTot = " ";
 
-    for (var i = 0; i < voto; i++){
+                for (var i = 0; i < voto; i++){
 
-        stelleTot += '<i id="gold" class="fas fa-star"></i>';
-    }
-    for (var i = 0 ; i < 5 - voto ;  i++ ) {
-        stelleTot += '<i id="yellow" class="far fa-star"></i>';
-    }
-    return stelleTot;
+                    stelleTot += '<i id="gold" class="fas fa-star"></i>';
+                }
+                for (var i = 0 ; i < 5 - voto ;  i++ ) {
+                    stelleTot += '<i id="yellow" class="far fa-star"></i>';
+                }
+                return stelleTot;
 }
